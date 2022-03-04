215fdf59ef86704c5a857eb3832d1a7d2e261967
YAMAGIRI SALEEMBASHA <95207243+yamagirisaleembasha@users.noreply.github.com>
2022-03-04 10:53:26 +0530

    10.BST and its operations, Traversals

---
+++ bst.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+        int data;
+        struct node* right;
+        struct node* left;
+};
+struct node* create(int x)
+{
+        struct node* root;
+        root=malloc(sizeof(struct node));
+        root->data=x;
+        root->left=NULL;
+        root->right=NULL;
+        return root;
+}
+struct node* insert(struct node* root,int x)
+{
+        if(root==NULL)
+                return create(x);
+        else if(x>root->data)
+                root->right=insert(root->right,x);
+        else
+                root->left=insert(root->left,x);
+        return root;
+}
+
+void inorder(struct node* root)
+{
+        if(root!=NULL)
+        {
+                inorder(root->left);
+                printf("%d\t",root->data);
+                inorder(root->right);
+        }
+}
+void postorder(struct node* root)
+{
+        if(root!=NULL)
+        {
+                postorder(root->left);
+                postorder(root->right);
+                printf("%d\t",root->data);
+        }
+}
+void preorder(struct node* root)
+{
+        if(root!=NULL)
+        {
+                printf("%d\t",root->data);
+                preorder(root->left);
+                preorder(root->right);
+        }
+}
+void main()
+{
+        struct node* root;
+        int n;
+        printf("enter number of nodes you want to insert in the tree:\n");
+        scanf("%d",&n);
+        int a[n],i;
+        printf("enter root node of the tree:\n");
+        scanf("%d",&a[0]);
+        printf("enter the other elements of your tree:\n");
+        for(i=1;i<n;i++)
+                scanf("%d",&a[i]);
+        root=create(a[0]);
+        for(i=1;i<n;i++)
+                insert(root,a[i]);
+        printf("preorder of the given tree is:\n");
+        preorder(root);
+        printf("\n");
+        printf("inorder of the given tree is:\n");
+        inorder(root);
+        printf("\n");
+        printf("postorder of the given tree is:\n");
+        postorder(root);
+        printf("\n");
+}
